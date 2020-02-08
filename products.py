8ed1c92614b6ee779a0936399f046967c482e021
Vic Kuo <eun.hru@gmail.com>
2020-02-08 12:45:52 +0800

    add save file

--- products.py
+++ products.py
@@ -9,3 +9,8 @@ print(products)
 
 for p in products:
 	print(p[0], '的價格是', p[1])
+
+#with = auto close file 
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
